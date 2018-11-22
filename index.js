56817e277d407d1e47a2bc59b727bcaea436094b
Dmitriy Tarabanov <dmitry.tarabanov@decimadigital.com>
2018-11-22 10:47:57 +0200

    test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config.js');
+const config = require('./config.js'); 
 const url = require("url");
 const {getMethod, postMethod, watchFs} = require('./myModules/tube.js')
 const http = require('http');
